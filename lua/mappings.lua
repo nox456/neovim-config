4bb8e54ea448e90ea3235d59a740516c4fda669f
Gabriel_Vieira <nox030705@gmail.com>
2024-10-08 09:25:51 -0400

    feat(mappings): added mappings file

---
+++ lua/mappings.lua
@@ -0,0 +1,13 @@
+local map = vim.keymap.set
+
+-- leader key
+vim.g.mapleader = ","
+
+-- Window mappings
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "Close window under the cursor" })
+map("n", "<leader>q!", "<cmd>q!<CR>", { desc = "Close window (forced) under the cursor" })
+
+-- Buffer mappings
+map("n", "<leader>s", "<cmd>w<CR>", { desc = "Save buffer under the cursor" })
+map("n", "<F2>", "<cmd>bprev<CR>", { desc = "Go to previous buffer" })
+map("n", "<F3>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
