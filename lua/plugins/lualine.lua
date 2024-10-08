3f91bef567025309c82e2afbaac74f621f1fce65
Gabriel_Vieira <nox030705@gmail.com>
2024-10-08 12:20:43 -0400

    feat(plugins): installed lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,44 @@
+local function time()
+	return " " .. os.date("%I:%M %p")
+end
+
+local function cwd()
+	return "  " .. vim.fn.fnamemodify(vim.fn.getcwd(),":t")
+end
+
+local function ip()
+local n = os.tmpname()
+ 
+	os.execute("ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1 > " .. n)
+  
+	local ip
+	for line in io.lines(n) do
+		ip = line
+	end
+	os.remove(n)
+
+	if ip == nil then
+		ip = "localhost"
+	end
+
+	return "󰍹  " ..  ip
+end
+
+return {
+	{
+    		'nvim-lualine/lualine.nvim',
+    		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		opts = {
+			theme = 'onedark',
+			sections = {
+				lualine_a = {'mode'},
+				lualine_b = {'branch','diff'},
+				lualine_c = {'diagnostics'},
+				lualine_x = { ip },
+				lualine_y= { cwd },
+				lualine_z= { time }
+			}
+
+		}
+	}
+}
