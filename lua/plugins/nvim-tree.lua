77e02b08caecba380f8fed7bb945172cb5d7f603
Gabriel_Vieira <nox030705@gmail.com>
2024-10-10 21:06:13 -0400

    chore(plugins): opts property in nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,24 +5,22 @@ return {
 	  dependencies = {
 	    "nvim-tree/nvim-web-devicons",
 	  },
-	  config = function()
-		require("nvim-tree").setup {
-		    sync_root_with_cwd = true,
-		    renderer = {
-			    root_folder_label = false,
-			    highlight_opened_files = "name"
-		    },
-		    filters = {
-			    dotfiles = true
+	  opts = {
+		sync_root_with_cwd = true,
+		renderer = {
+		    root_folder_label = false,
+		    highlight_opened_files = "name"
+		},
+		filters = {
+		    dotfiles = true
+		},
+		actions = {
+			change_dir = {
+				global = true
 			},
-			actions = {
-				change_dir = {
-					global = true
-				},
-				open_file = {
-					quit_on_open = true
-				}
+			open_file = {
+				quit_on_open = true
 			}
 		}
-	  end,
+	  }
 }
