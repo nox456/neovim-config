4452bd20d3914f2f7e0aa9da597d411a56b4c7e3
Gabriel_Vieira <nox030705@gmail.com>
2024-10-19 12:08:56 -0400

    feat(dashboard): open dashboard when close buffers

--- lua/plugins/dashboard-nvim.lua
+++ lua/plugins/dashboard-nvim.lua
@@ -1,3 +1,26 @@
+vim.api.nvim_create_autocmd('BufDelete',{
+	callback = function()
+		local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(0))
+		local windows = #vim.api.nvim_list_wins()
+		if bufname == "" and windows == 1 then
+			vim.o.showtabline = 0
+			vim.cmd("Dashboard")
+		end
+	end
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(0))
+		local windows = #vim.api.nvim_list_wins()
+		if not string.find(bufname, "NvimTree_1") and bufname ~= "" then
+			if vim.o.showtabline == 0 then
+				vim.o.showtabline = 2
+			end
+		elseif bufname == "" and windows == 1 then
+			vim.o.showtabline = 0
+		end
+	end
+})
 return {
 	'nvimdev/dashboard-nvim', 
 	event = "VimEnter",
@@ -6,6 +29,9 @@ return {
 		theme = 'doom',
 		shortcut_type = 'number',
 		disable_move = true,
+		hide = {
+			tabline = false
+		},
 		config = {
 			header = {
 				'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
