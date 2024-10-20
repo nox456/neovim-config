26321f985a24f6a63e344ea75d2379f590fee7f9
Gabriel_Vieira <nox030705@gmail.com>
2024-10-20 14:03:37 -0400

    feat(plugins): installed nvim-treesitter plugin

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,29 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = { 
+				"html",
+				"css",
+				"gitcommit",
+				"gitignore",
+				"http",
+				"javascript",
+				"json",
+				"jsdoc",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"sql"
+			},
+			sync_install = false,
+			highlight = {
+				enable = true
+			}
+		})
+	end
+}
