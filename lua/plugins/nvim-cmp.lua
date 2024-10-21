d8da69c80144dc4b006ee0377727720d7632d2f3
Gabriel_Vieira <nox030705@gmail.com>
2024-10-20 21:37:55 -0400

    feat(cmp): better completion menu

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,17 +1,26 @@
-
-
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
+		"onsails/lspkind.nvim",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline"
 	},
 	config = function()
 		local cmp = require("cmp")
+		local lspkind = require("lspkind")
 
 		cmp.setup({
+			formatting = {
+				format = lspkind.cmp_format({
+					mode = "symbol_text",
+					maxwidth = 30
+				})
+			},
+			window = {
+				completion = cmp.config.window.bordered()
+			},
 			sources = {
 				{ name = "buffer" },
 				{ name = "path" },
