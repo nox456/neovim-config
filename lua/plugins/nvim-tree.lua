b2f659adfcd68689df96c57f445a60f79911c8aa
Gabriel_Vieira <nox030705@gmail.com>
2024-10-25 12:11:39 -0400

    feat(nvim-tree): changed 'CD' mapping

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,18 @@
+local function my_on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    vim.keymap.set("n", "<C-CR>", api.tree.change_root_to_node, { 
+        desc = "CD",
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true
+    })
+
+end
+
 return {
 	  "nvim-tree/nvim-tree.lua",
 	  version = "*",
@@ -6,6 +21,7 @@ return {
 	    "nvim-tree/nvim-web-devicons",
 	  },
 	  opts = {
+        on_attach = my_on_attach,
 		sync_root_with_cwd = true,
 		renderer = {
 		    root_folder_label = false,
