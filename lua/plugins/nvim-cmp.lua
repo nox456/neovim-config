5b550f1c27ac48cd8b4209d21424592e1d9342eb
Gabriel_Vieira <nox030705@gmail.com>
2024-10-30 00:04:11 -0400

    feat(plugins): installed 'nvim-autopairs' plugin

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,35 +5,37 @@ return {
 		"onsails/lspkind.nvim",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-        "hrsh7th/cmp-nvim-lsp"
+		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
 		local cmp = require("cmp")
 		local lspkind = require("lspkind")
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 		cmp.setup({
-            completion = {
-                completeopt = "menu,menuone"
-            },
+			completion = {
+				completeopt = "menu,menuone",
+			},
 			formatting = {
 				format = lspkind.cmp_format({
 					mode = "symbol_text",
-					maxwidth = 30
-				})
+					maxwidth = 30,
+				}),
 			},
 			window = {
-				completion = cmp.config.window.bordered()
+				completion = cmp.config.window.bordered(),
 			},
 			sources = {
-                { name = "nvim_lsp" },
+				{ name = "nvim_lsp" },
 				{ name = "buffer" },
-				{ name = "path" }
+				{ name = "path" },
 			},
 			mapping = {
-				['<Tab>'] = cmp.mapping.select_next_item(),
-				['<S-Tab>'] = cmp.mapping.select_prev_item(),
-				['<CR>'] = cmp.mapping.confirm({ select = true })
-			}
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			},
 		})
 	end,
 }
