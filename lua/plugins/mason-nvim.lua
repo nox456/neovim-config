c3582cb8fc5b7c9a47845d5bbd25b29450f0c9e7
Gabriel_Vieira <nox030705@gmail.com>
2024-11-01 12:18:58 -0400

    feat(mason): enable css completion

--- lua/plugins/mason-nvim.lua
+++ lua/plugins/mason-nvim.lua
@@ -1,22 +1,34 @@
 return {
 	"williamboman/mason.nvim",
-    lazy = false,
-    dependencies = {
+	lazy = false,
+	dependencies = {
 		"neovim/nvim-lspconfig",
-    },
-    config = function()
-            require("mason").setup()
-            local servers = {
-                "pylsp",
-                "html",
-                "cssls",
-                "emmet_language_server",
-                "jsonls",
-                "ts_ls",
-                "lua_ls"
-            }
-            for _,server in pairs(servers) do
-                require("lspconfig")[server].setup({})
-            end
-    end
+	},
+	config = function()
+		require("mason").setup()
+		local servers = {
+			"pylsp",
+			"html",
+			"cssls",
+			"emmet_language_server",
+			"jsonls",
+			"ts_ls",
+			"lua_ls",
+		}
+		for _, server in pairs(servers) do
+			require("lspconfig")[server].setup({})
+		end
+        -- Exclude emmet in css files
+		require("lspconfig").emmet_language_server.setup({
+			init_options = {
+				excludeLanguages = { "css" },
+			},
+		})
+        -- Enable completion in css
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
+		require("lspconfig").cssls.setup({
+			capabilities = capabilities,
+		})
+	end,
 }
