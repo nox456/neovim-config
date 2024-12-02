45991dbfacc78b62ccfd0fe5dfba6043e95dafae
Gabriel_Vieira <nox030705@gmail.com>
2024-12-02 12:27:27 -0400

    feat(mappings): remap comments from 'gcc' to '<leader>k'

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -9,36 +9,41 @@ map("n", "<leader>s", "<cmd>w<CR>", { desc = "Save buffer under the cursor" })
 map("n", "<F2>", "<cmd>bprev<CR>", { desc = "Go to previous buffer" })
 map("n", "<F3>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
 map("n", "<leader>w", "<cmd>Bdelete<CR>", { desc = "Close buffer under the cursor" })
-map("n", "<leader>T", "<cmd>tabnew<CR>", { desc = "Create a new tab" } )
+map("n", "<leader>T", "<cmd>tabnew<CR>", { desc = "Create a new tab" })
 map("n", "<F5>", "<cmd>tabnext<CR>", { desc = "Got to next tab" })
 map("n", "<F4>", "<cmd>tabprevious<CR>", { desc = "Go to previous tab" })
 map("n", "<leader>Q", "<cmd>tabclose<CR>", { desc = "Close actual tab" })
 
 -- Nvim-tree
-map("n","<leader>b","<cmd>NvimTreeToggle<CR>", { desc = "Show file explorer" } )
+map("n", "<leader>b", "<cmd>NvimTreeToggle<CR>", { desc = "Show file explorer" })
 
 -- Format Buffer
 map("n", "<leader>i", function()
-    require("conform").format({ async = true })
-end, { desc = "Format the buffer under the cursor"})
+	require("conform").format({ async = true })
+end, { desc = "Format the buffer under the cursor" })
 
 -- Telescope
-map("n", "<leader>f", "<cmd>Telescope live_grep<CR>", { desc = "Find words in workspace "})
-map("n", "<leader>P", "<cmd>Telescope project<CR>", { desc  = "Open projects manager "})
+map("n", "<leader>f", "<cmd>Telescope live_grep<CR>", { desc = "Find words in workspace " })
+map("n", "<leader>P", "<cmd>Telescope project<CR>", { desc = "Open projects manager " })
 
 -- Terminals
-map("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { desc = "Open horizontal terminal"})
-map("n", "<leader>tv", ":ToggleTerm size=40 direction=vertical<CR>", { desc = "Open vertical terminal"})
-map("n", "<leader>tf", "<cmd>ToggleTerm direction=float name=Terminal<CR>", { desc = "Open floating terminal"})
-map("n", "<leader>tt", "<cmd>ToggleTerm direction=tab name=Terminal<CR>", { desc = "Open full terminal in tab "})
+map("n", "<leader>th", ":ToggleTerm direction=horizontal<CR>", { desc = "Open horizontal terminal" })
+map("n", "<leader>tv", ":ToggleTerm size=40 direction=vertical<CR>", { desc = "Open vertical terminal" })
+map("n", "<leader>tf", "<cmd>ToggleTerm direction=float name=Terminal<CR>", { desc = "Open floating terminal" })
+map("n", "<leader>tt", "<cmd>ToggleTerm direction=tab name=Terminal<CR>", { desc = "Open full terminal in tab " })
 map("t", "<leader><Esc>", "<C-\\><C-N>", { desc = "Espace from terminal mode" })
 
 -- Git
-map("n", "<leader>g", "<cmd>Neogit<CR>", { desc = "Open Git Manager" } )
+map("n", "<leader>g", "<cmd>Neogit<CR>", { desc = "Open Git Manager" })
 
 -- Misc
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Folds
-map("n", "<leader><","zc",{ desc = "Fold", remap = true })
-map("n", "<leader>>","za",{ desc = "Unfold", remap = true })
+map("n", "<leader><", "zc", { desc = "Fold", remap = true })
+map("n", "<leader>>", "za", { desc = "Unfold", remap = true })
+
+-- Comment
+map("n", "<leader>k", "gcc", { desc = "Comment", remap = true })
+map("v", "<leader>k", "gc", { desc = "Comment", remap = true })
+
