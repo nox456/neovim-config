37f768cd779eda96162c9b6cadab442d57a15936
Gabriel Vieira <nox030705@gmail.com>
2025-01-04 00:11:26 -0400

    feat(mason): added 'volar' (vue lsp) configuration

--- lua/plugins/mason-nvim.lua
+++ lua/plugins/mason-nvim.lua
@@ -14,21 +14,36 @@ return {
 			"jsonls",
 			"ts_ls",
 			"lua_ls",
+			"jdtls",
+			"volar",
 		}
 		for _, server in pairs(servers) do
-			require("lspconfig")[server].setup({})
+			require("lspconfig")[server].setup({
+				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+			})
 		end
-        -- Exclude emmet in css files
+		-- Exclude emmet in css files
 		require("lspconfig").emmet_language_server.setup({
 			init_options = {
 				excludeLanguages = { "css" },
 			},
 		})
-        -- Enable completion in css
+		-- Enable completion in css
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
 		require("lspconfig").cssls.setup({
 			capabilities = capabilities,
+		})
+		require("lspconfig").volar.setup({
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+			init_options = {
+				vue = {
+					hybridMode = false,
+				},
+				typescript = {
+					tsdk = "/home/nox/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript/lib",
+				},
+			},
 		})
 	end,
 }
