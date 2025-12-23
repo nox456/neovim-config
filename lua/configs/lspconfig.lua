25efdddaa98dc4023e423ebfc8edc90372338a67
Gabriel Vieira <nox030705@gmail.com>
2025-12-23 17:35:14 -0400

    feat(lsp): added astro lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "ts_ls", "eslint", "cssls", "html", "hyprls", "jsonls", "biome", "prismals", "gh_actions_ls" }
+local servers = { "ts_ls", "eslint", "cssls", "html", "hyprls", "jsonls", "biome", "prismals", "gh_actions_ls", "astro" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
